6a0b48ffacd7e1dbdbe9b8a3c03886981ef05ceb
mytinylabo <mytinylabo@gmail.com>
2019-10-18 20:54:33 +0900

    grading of chapter 0 done

--- grading/inverse_index_lab.py
+++ grading/inverse_index_lab.py
@@ -1,22 +1,22 @@
 # version code 80e56511a793+
 # Please fill out this stencil and submit using the provided submission script.
 
+from functools import reduce
+from random import randint
 
 
-
-
-## 1: (Task 0.6.2) Movie Review
-## Task 1
+# 1: (Task 0.6.2) Movie Review
+# Task 1
 def movie_review(name):
     """
     Input: the name of a movie
     Output: a string (one of the review options), selected at random using randint
     """
-    return ...
+    reviews = ["See it!", "A gem!", "Ideological claptrap!"]
+    return reviews[randint(0, len(reviews) - 1)]
 
 
-
-## 2: (Task 0.6.6) Make Inverse Index
+# 2: (Task 0.6.6) Make Inverse Index
 def makeInverseIndex(strlist):
     """
     Input: a list of documents as strings
@@ -31,17 +31,18 @@ def makeInverseIndex(strlist):
     >>> makeInverseIndex(['hello world','hello','hello cat','hellolot of cats']) == {'hello': {0, 1, 2}, 'cat': {2}, 'of': {3}, 'world': {0}, 'cats': {3}, 'hellolot': {3}}
     True
     """
-    pass
+    words = set(' '.join(strlist).split())
+    docs_with_id = list(enumerate(strlist))
+    return {w: {i for i, doc in docs_with_id if w in doc} for w in words}
 
 
-
-## 3: (Task 0.6.7) Or Search
+# 3: (Task 0.6.7) Or Search
 def orSearch(inverseIndex, query):
     """
     Input: an inverse index, as created by makeInverseIndex, and a list of words to query
     Output: the set of document ids that contain _any_ of the specified words
     Feel free to use a loop instead of a comprehension.
-    
+
     >>> idx = makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
     >>> orSearch(idx, ['Bach','the'])
     {0, 2, 3, 4, 5}
@@ -52,11 +53,10 @@ def orSearch(inverseIndex, query):
     >>> idx == makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
     True
     """
-    pass
+    return reduce(lambda acc, x: acc | x, [inverseIndex[q] for q in query])
 
 
-
-## 4: (Task 0.6.8) And Search
+# 4: (Task 0.6.8) And Search
 def andSearch(inverseIndex, query):
     """
     Input: an inverse index, as created by makeInverseIndex, and a list of words to query
@@ -73,5 +73,4 @@ def andSearch(inverseIndex, query):
     >>> idx == makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
     True
     """
-    pass
-
+    return reduce(lambda acc, x: acc & x, [inverseIndex[q] for q in query])
