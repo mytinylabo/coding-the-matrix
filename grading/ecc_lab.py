3a80cdcbfb27ccf6d4735b0cf07bdf6c0e2343b8
mytinylabo <mytinylabo@gmail.com>
2019-12-02 04:09:17 +0900

    chapter 4 WIP

--- grading/ecc_lab.py
+++ grading/ecc_lab.py
@@ -1,33 +1,36 @@
 # version code 80e56511a793+
 # Please fill out this stencil and submit using the provided submission script.
 
+from matutil import coldict2mat, mat2coldict
 from vec import Vec
 from mat import Mat
 from bitutil import bits2mat, str2bits, noise
 from GF2 import one
 
-## Task 1
+# Task 1
 """ Create an instance of Mat representing the generator matrix G. You can use
 the procedure listlist2mat in the matutil module (be sure to import first).
 Since we are working over GF (2), you should use the value one from the
 GF2 module to represent 1"""
 
 from matutil import listlist2mat
-G = listlist2mat([[one,0,one,one],[one,one,0,one],[0,0,0,one],[one,one,one,0],[0,0,one,0],[0,one,0,0],[one,0,0,0]])
+G = listlist2mat([[one, 0, one, one], [one, one, 0, one], [0, 0, 0, one], [one, one, one, 0], [0, 0, one, 0], [0, one, 0, 0], [one, 0, 0, 0]])
 
-## Task 2
+# Task 2
 # Please write your answer as a list. Use one from GF2 and 0 as the elements.
-encoding_1001 = [0,0,one,one,0,0,one]
+encoding_1001 = [0, 0, one, one, 0, 0, one]
 
-## Task 3
+# Task 3
 # Express your answer as an instance of the Mat class.
-R = None
+R = listlist2mat([[0, 0, 0, 0, 0, 0, one], [0, 0, 0, 0, 0, one, 0], [0, 0, 0, 0, one, 0, 0], [0, 0, one, 0, 0, 0, 0]])
 
-## Task 4
+# Task 4
 # Create an instance of Mat representing the check matrix H.
-H = None
+H = listlist2mat([[0, 0, 0, one, one, one, one], [0, one, one, 0, 0, one, one], [one, 0, one, 0, one, 0, one]])
 
-## Task 5
+# Task 5
+
+
 def find_error(syndrome):
     """
     Input: an error syndrome as an instance of Vec
@@ -42,17 +45,21 @@ def find_error(syndrome):
         >>> find_error(Vec({0,1,2}, {})) == Vec({0,1,2,3,4,5,6}, {})
         True
     """
-    pass
+    n = sum([2**i if syndrome[2 - i] == one else 0 for i in range(3)])
+    return Vec(set(range(7)), {n - 1: one} if n > 0 else dict())
 
-## Task 6
+
+# Task 6
 # Use the Vec class for your answers.
-non_codeword = Vec({0,1,2,3,4,5,6}, {0: one, 1:0, 2:one, 3:one, 4:0, 5:one, 6:one})
-error_vector = Vec(..., ...)
-code_word = Vec(..., ...)
-original = ... # R * code_word
+non_codeword = Vec({0, 1, 2, 3, 4, 5, 6}, {0: one, 1: 0, 2: one, 3: one, 4: 0, 5: one, 6: one})
+error_vector = Vec({0, 1, 2, 3, 4, 5, 6}, {6: one})
+code_word = Vec({0, 1, 2, 3, 4, 5, 6}, {0: one, 1: 0, 2: one, 3: one, 4: 0, 5: one, 6: 0})
+original = Vec({0, 1, 2, 3}, {0: 0, 1: one, 2: 0, 3: one})  # R * code_word
 
 
-## Task 7
+# Task 7
+
+
 def find_error_matrix(S):
     """
     Input: a matrix S whose columns are error syndromes
@@ -62,22 +69,25 @@ def find_error_matrix(S):
         >>> find_error_matrix(S) == Mat(({0, 1, 2, 3, 4, 5, 6}, {0, 1, 2, 3}), {(1, 3): 0, (3, 0): 0, (2, 1): 0, (6, 2): 0, (5, 1): one, (0, 3): 0, (4, 0): 0, (1, 2): 0, (3, 3): 0, (6, 3): 0, (5, 0): 0, (2, 2): 0, (4, 1): 0, (1, 1): 0, (3, 2): one, (0, 0): 0, (6, 0): 0, (2, 3): 0, (4, 2): 0, (1, 0): 0, (5, 3): 0, (0, 1): 0, (6, 1): 0, (3, 1): 0, (2, 0): 0, (4, 3): one, (5, 2): 0, (0, 2): 0})
         True
     """
-    pass
+    return coldict2mat({col: find_error(s) for col, s in sorted(mat2coldict(S).items(), key=lambda p: p[0])})
 
-## Task 8
+
+# Task 8
 s = "I'm trying to free your mind, Neo. But I can only show you the door. You're the one that has to walk through it."
-P = None
+P = bits2mat(str2bits(s))
 
-## Task 9
-C = None
-bits_before = None
-bits_after = None
+# Task 9
+C = G * P
+bits_before = len(s) * 8
+bits_after = len(s) * 14
 
 
-## Ungraded Task
-CTILDE = None
+# Ungraded Task
+CTILDE = C + noise(C, 0.02)
 
-## Task 10
+# Task 10
+
+
 def correct(A):
     """
     Input: a matrix A each column of which differs from a codeword in at most one bit
@@ -87,5 +97,4 @@ def correct(A):
         >>> correct(A) == Mat(({0, 1, 2, 3, 4, 5, 6}, {1, 2, 3}), {(0, 1): 0, (1, 2): 0, (3, 2): 0, (1, 3): 0, (3, 3): 0, (5, 2): one, (6, 1): 0, (3, 1): 0, (2, 1): 0, (0, 2): one, (6, 3): one, (4, 2): 0, (6, 2): one, (2, 3): 0, (4, 3): 0, (2, 2): 0, (5, 1): 0, (0, 3): one, (4, 1): 0, (1, 1): 0, (5, 3): one})
         True
     """
-    pass
-
+    return A + find_error_matrix(H * A)
